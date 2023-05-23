b4faa7e9a846af77e15173cd8317f7b33099998a
Jordan Kapelner <jordanka@optonline.net>
2023-05-23 10:33:31 -0400

    fixed personal info

--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 import {CrudSaveComponent} from "../_directives";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Location} from "@angular/common";
-import {AlertService, CrudService, PersonalInfoService, StorageService, UserService} from "../_services";
+import {AlertService, PersonalInfoService, StorageService, UserService} from "../_services";
 import {FormBuilder, Validators} from "@angular/forms";
 import {User} from "../_models";
 
@@ -17,13 +17,13 @@ export class PersonalInfoComponent extends CrudSaveComponent {
     protected router: Router,
     protected location: Location,
     protected route: ActivatedRoute,
-    protected crudService: PersonalInfoService,
+    protected infoService: PersonalInfoService,
     protected alertService: AlertService,
     protected formBuilder: FormBuilder,
     protected userService: UserService,
     protected storageService: StorageService
   ) {
-    super(router, location, route, crudService, alertService);
+    super(router, location, route, infoService, alertService);
     this.isNew = false
     this.objectName = 'personal info'
     this.title = 'Personal Info'
@@ -33,17 +33,12 @@ export class PersonalInfoComponent extends CrudSaveComponent {
     this.form = this.formBuilder.group({
       first_name: [null, [Validators.required]],
       last_name: [null, [Validators.required]],
-      email: [null, [Validators.required]],
-      phone: [null, null]
     })
     this.userService.getCurrent()
       .subscribe((user: User) => {
         this.id = user.id;
-        if (user.is_staff && user.account.owner_id) {
-          this.id = user.account.owner_id
-        }
         this.user = user
-        this.crudService.get(this.id)
+        this.infoService.get(this.id)
           .subscribe((data: User) => {
             this._data$.next(data)
             this.form.patchValue(data);
@@ -54,10 +49,7 @@ export class PersonalInfoComponent extends CrudSaveComponent {
   }
 
   protected onSaveComplete(data) {
-    if (!this.user.is_staff) {
-      this.storageService.set('currentUser', {...this.user, ...data})
-      window.location.reload()
-    }
+    this.storageService.set('currentUser', {...this.user, ...data})
     this.loading = false
   }
 }
